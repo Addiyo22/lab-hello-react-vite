def613a97bbe2106f47c47eab110129a9dd8716a
Aditya Raikar <raikaraditya22@gmail.com>
2024-08-06 23:13:08 +0200

    done

---
+++ demo-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from './components/nav/nav.jsx'
+import Body from './components/body/body.jsx'
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Nav />
+        <Body />
+      </div>
+    </>
+  )
+}
+
+export default App
